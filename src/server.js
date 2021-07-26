ca4a39cf2aa9e31892ab68bcd5621617f0186d09
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-07-26 19:33:34 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,8 +9,3 @@ server.use(express.json())
 server.use(app)
 
 server.listen(port, err => !err ? console.log('server is running...'): console.log("server is not running..."))
-
-
-batata
-mongo
-ferrou!
